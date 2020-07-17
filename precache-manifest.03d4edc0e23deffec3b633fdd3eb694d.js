15c84c8800ba7c4e75e06cb0a1d7aaf4b4a0b3a3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-17 06:09:54 +0000

    Deploy Rhapsody0128/Vanessa to github.com/Rhapsody0128/Vanessa.git:gh-pages

---
+++ precache-manifest.03d4edc0e23deffec3b633fdd3eb694d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd01853c282c85ac173e",
+    "url": "/css/app.9c249556.css"
+  },
+  {
+    "revision": "d3acd3904ff69ba95833",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "ea6653836b23225a8af7",
+    "url": "/css/member.34444efe.css"
+  },
+  {
+    "revision": "bb2d5999c539aa418d6b",
+    "url": "/css/member_login.34444efe.css"
+  },
+  {
+    "revision": "fad9896086d942d3035a",
+    "url": "/css/menu.51321ab7.css"
+  },
+  {
+    "revision": "32a669030433d7c51e96",
+    "url": "/css/news.427ebcd9.css"
+  },
+  {
+    "revision": "d306f319d30602f84975",
+    "url": "/css/story.f0ff05ef.css"
+  },
+  {
+    "revision": "e31926857dbfad55a6edf1f581207b91",
+    "url": "/img/1472660456_1e1841d24e_o.jpg"
+  },
+  {
+    "revision": "937c953b7fed26dd5cce99ab225402f4",
+    "url": "/img/36271782954_f7aa950180_o.jpg"
+  },
+  {
+    "revision": "5d0d1bd2eeae462f6449151011132168",
+    "url": "/img/36420482760_fe0771f10d_o.jpg"
+  },
+  {
+    "revision": "532151436fd98ef71a63d693154481f0",
+    "url": "/img/36420486130_f1c3388269_o.jpg"
+  },
+  {
+    "revision": "62d8e2e856983f1862a5de49779c4a79",
+    "url": "/img/4903092900_ff43be2aff_o.jpg"
+  },
+  {
+    "revision": "c506a968bbb7415ff979be66d8bfa3d1",
+    "url": "/img/9677717700_3b10b4f206_o.jpg"
+  },
+  {
+    "revision": "c9656f2b04b1c0e38593afab03236e0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd01853c282c85ac173e",
+    "url": "/js/app.20bfbb27.js"
+  },
+  {
+    "revision": "d3acd3904ff69ba95833",
+    "url": "/js/chunk-vendors.6398253f.js"
+  },
+  {
+    "revision": "0df43234ec287203607f",
+    "url": "/js/location.38ae20d7.js"
+  },
+  {
+    "revision": "ea6653836b23225a8af7",
+    "url": "/js/member.a4bd3864.js"
+  },
+  {
+    "revision": "bb2d5999c539aa418d6b",
+    "url": "/js/member_login.88881858.js"
+  },
+  {
+    "revision": "fad9896086d942d3035a",
+    "url": "/js/menu.2cc033c9.js"
+  },
+  {
+    "revision": "32a669030433d7c51e96",
+    "url": "/js/news.fad7c9ea.js"
+  },
+  {
+    "revision": "d306f319d30602f84975",
+    "url": "/js/story.5c354ebd.js"
+  },
+  {
+    "revision": "6c086cc07b31a346ecdc3f79c40418ec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
